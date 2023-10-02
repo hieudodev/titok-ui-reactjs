e1d1cc2bd37385edfce5a9451e398f823fb744c3
hieudodev <hieudutqn@gmail.com>
2023-10-02 10:01:38 +0700

    css header search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,37 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import images from '~/assets/images';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+console.log(images.logo.default);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* Logo */}
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="titok" />
+                </div>
                 {/* Search */}
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    {/* <button className={cx('loading')}>
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </button> */}
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <button>Đăng ký</button>
+                    <button>Đăng nhập</button>
+                </div>
             </div>
         </header>
     );
